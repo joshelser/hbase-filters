c420513692acf8a59bfa7d5fddb3d0d87839eaa6
Josh Elser <josh.elser@gmail.com>
2017-02-07 12:55:10 -0500

    Initial version showing how to use SingleColumnValueFilters with a FilterList

---
+++ src/main/java/com/github/joshelser/hbase/Filters.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.joshelser.hbase;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
+import org.apache.hadoop.hbase.filter.FilterList;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class Filters implements Runnable {
+  private static final String TABLE_NAME = "JoshHBaseFiltersTest";
+  private static final byte[] FAM1 = Bytes.toBytes("f1");
+  private static final byte[] FAM2 = Bytes.toBytes("f2");
+
+  public Filters() {}
+
+  @Override
+  public void run() {
+    try {
+      _run();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  void _run() throws Exception {
+    Configuration conf = new Configuration(true);
+    conf.addResource(new Path("/usr/local/lib/hbase/conf/hbase-site.xml"));
+    Connection conn = ConnectionFactory.createConnection(conf);
+
+    TableName tn = getOrCreateTable(conn);
+
+    // Should see rows1 and 3
+    writeData(conn, tn ,"row1", true, true);
+    writeData(conn, tn ,"row2", false, false);
+    writeData(conn, tn ,"row3", true, true);
+    writeData(conn, tn ,"row4", true, false);
+
+    Set<String> rowsSeen = new HashSet<>();
+    Scan scan = createScan();
+    try (Table t = conn.getTable(tn)) {
+      ResultScanner rs = t.getScanner(scan);
+      System.out.println("*** Reading results");
+      for (Result result : rs) {
+        rowsSeen.add(new String(result.getRow(), StandardCharsets.UTF_8));
+        System.out.println(result.toString());
+      }
+      System.out.println("*** Done reading results");
+    }
+    if (rowsSeen.size() != 2 || !rowsSeen.contains("row1") | !rowsSeen.contains("row3")) {
+      throw new RuntimeException("Unexpected results. Saw rows: " + rowsSeen + ", should have seen {row1, row3}");
+    }
+    System.out.println("Success");
+  }
+
+  TableName getOrCreateTable(Connection conn) throws Exception {
+    Admin admin = conn.getAdmin();
+    TableName[] tableNames = admin.listTableNames();
+    for (TableName tableName : tableNames) {
+      if (tableName.getNameAsString().equals(TABLE_NAME)) {
+        return tableName;
+      }
+    }
+    TableName tn = TableName.valueOf(TABLE_NAME);
+    HTableDescriptor htd = new HTableDescriptor(tn);
+    htd.addFamily(new HColumnDescriptor(FAM1));
+    htd.addFamily(new HColumnDescriptor(FAM2));
+    admin.createTable(htd);
+    return tn;
+  }
+
+  void writeData(Connection conn, TableName tn, String row, boolean yes1, boolean yes2) throws Exception {
+    Put p = new Put(Bytes.toBytes(row));
+    p.addColumn(FAM1, Bytes.toBytes("col1"), Bytes.toBytes("whatever"));
+    p.addColumn(FAM2, Bytes.toBytes("col1"), (yes1 ? Bytes.toBytes("YES") : Bytes.toBytes("NO")));
+    p.addColumn(FAM2, Bytes.toBytes("col2"), (yes2 ? Bytes.toBytes("YES") : Bytes.toBytes("NO")));
+    Table t = conn.getTable(tn);
+    t.put(p);
+    t.close();
+  }
+
+  Scan createScan() {
+    Scan scan = new Scan();
+    FilterList list = new FilterList(FilterList.Operator.MUST_PASS_ALL);
+    SingleColumnValueFilter cvFilter1 = new SingleColumnValueFilter(FAM2, Bytes.toBytes("col1"), CompareOp.EQUAL, Bytes.toBytes("YES"));
+    SingleColumnValueFilter cvFilter2 = new SingleColumnValueFilter(FAM2, Bytes.toBytes("col2"), CompareOp.EQUAL, Bytes.toBytes("YES"));
+    list.addFilter(cvFilter1);
+    list.addFilter(cvFilter2);
+    scan.addColumn(FAM2, Bytes.toBytes("col1"));
+    scan.addColumn(FAM2, Bytes.toBytes("col2"));
+    scan.setFilter(list);
+    return scan;
+  }
+
+  public static void main(String[] args) throws Exception {
+    new Filters().run();
+  }
+}
